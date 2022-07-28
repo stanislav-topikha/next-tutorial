4d2271bca4f4b2211ad543e51fd370604bb67746
stanislav-t <stanislav.topikha@gmail.com>
2022-07-28 10:31:08 +0300

    From React to Next.js

---
+++ pages/Header.tsx
@@ -0,0 +1,3 @@
+export function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Header } from './Header';
+
+export default function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 2);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
